b8fb799759a7c8b3f2e57f44647ea3401ef899b4
Дмитрий Самсонов <NomekopSD@yandex.ru>
2018-08-14 21:10:06 +0300

    HW4: * Task 2 a * Task 2 b

---
+++ GeekBrains.CSharpSecond/HW4.Task2/MyExtansion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Task2
+{
+  public static class MyExtansion
+  {
+    public static Dictionary<int, int> CountExsampes(this IEnumerable<int> Col)
+    {
+      Dictionary<int, int> dict = new Dictionary<int, int>();
+      foreach (int item in Col)
+      {
+        if (dict.ContainsKey(item))
+          dict[item]++;
+        else
+          dict[item] = 1;
+      }
+      return dict;
+    }
+
+    public static Dictionary<T, int> CountExsampes<T>(this IEnumerable<T> Col)
+    {
+      Dictionary<T, int> dict = new Dictionary<T, int>();
+      foreach (T item in Col)
+      {
+        if (dict.ContainsKey(item))
+          dict[item]++;
+        else
+          dict[item] = 1;
+      }
+      return dict;
+    }
+  }
+}
---
+++ GeekBrains.CSharpSecond/HW4.Task2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Task2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      #region int count
+      List<int> L = new List<int>();
+      Random rnd = new Random(30);
+
+      for (int i = 0; i < 100; i++)
+      {
+        int x = rnd.Next(0, 10);
+        L.Add(x);
+        //Console.WriteLine(x);
+      }
+      Console.WriteLine();
+      
+      Console.WriteLine("Collection<int>");
+      var dict1 = L.CountExsampes();
+
+      foreach (var item in dict1)
+      {
+        Console.WriteLine(item);
+      }
+      Console.WriteLine();
+      #endregion
+      #region T count
+
+      Console.WriteLine("Collection<T>");
+      var dict2 = L.CountExsampes<int>();
+
+      foreach (var item in dict2)
+      {
+        Console.WriteLine(item);
+      }
+      Console.WriteLine();
+
+      Queue<string> Q = new Queue<string>();
+
+
+      for (int i = 0; i < 3; i++)
+      {
+        Q.Enqueue("Abra");
+        Q.Enqueue("Cod");
+        Q.Enqueue("Cod");
+        Q.Enqueue("CodAbra");
+        Q.Enqueue("CodAbra");
+        Q.Enqueue("CodAbra");
+      }
+      Console.WriteLine();
+
+      var dict3 = Q.CountExsampes<string>();
+
+      foreach (var item in dict3)
+      {
+        Console.WriteLine(item);
+      }
+      Console.WriteLine();
+      #endregion
+      #region Linq count
+
+      
+      //Console.WriteLine("Collection<T>");
+      //var dict4 = L.CountExsampes<int>();
+
+      //foreach (var item in dict4)
+      //{
+      //  Console.WriteLine(item);
+      //}
+      //Console.WriteLine();
+      
+      //var dict5 = Q.CountExsampes<string>();
+
+      //foreach (var item in dict5)
+      //{
+      //  Console.WriteLine(item);
+      //}
+      //Console.WriteLine();
+      #endregion
+
+      return;
+    }
+  }
+}
